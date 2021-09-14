826dde2f438eb9fda59577f5c89a0b6a2fde2b64
Peter <eskandar.peter@accenture.com>
2021-09-14 10:54:31 +0200

    test pipeline

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
+              echo "Hello, Foglietta" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
 }
