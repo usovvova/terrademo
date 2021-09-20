fee78314131c24c92e2ae0ef189ad4b0e88c8862
bhavik makadia <bhavik_makadia@outlook.com>
2021-09-20 21:24:12 +1000

    update TFC details

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "ACG-Terraform-Labs_100"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "testing-github-actions"
     }
   }
 }
