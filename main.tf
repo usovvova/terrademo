7a3dbd79f59963add9dcb5288131253e2592af15
stuart <stuart.kanauros@gmail.com>
2021-10-05 16:36:27 +0100

    updated backend

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "Solutions_As_A_servicec"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "lab-demo-github"
     }
   }
 }
