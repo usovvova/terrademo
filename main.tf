6d7a9b600731434b0251f35df6afb4834b47f8df
traian <madalinmerfu@gmail.com>
2021-10-15 14:44:32 +0200

    Main formatted code

--- main.tf
+++ main.tf
@@ -34,14 +34,14 @@ module "vpc" {
   name = "traian-my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b"]
+  azs            = ["us-east-1a", "us-east-1b"]
   public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
 
   enable_nat_gateway = false
   enable_vpn_gateway = false
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
@@ -51,8 +51,8 @@ resource "aws_instance" "web" {
   ami                    = "ami-09e67e426f25ce0d7"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-  availability_zone = module.vpc.public_subnets
-  user_data = <<-EOF
+  availability_zone      = module.vpc.public_subnets
+  user_data              = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
