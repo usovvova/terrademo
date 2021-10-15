7683019608e4c24adcfa5d18d190f58bfcbc7b5b
traian <madalinmerfu@gmail.com>
2021-10-15 21:40:00 +0200

    AZ 4 EC2

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aws_instance" "web" {
   ami                    = "ami-09e67e426f25ce0d7"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-  availability_zone      = module.vpc.public_subnets[0]
+  availability_zone      = module.vpc.azs[0]
   user_data              = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
