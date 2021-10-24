c1b48f89ef42e09f96be5c502f23af0fafda92de
Fabiano Prospero <fabiano.prospero@itility.nl>
2021-10-24 09:27:36 +0200

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "fpsouza"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "tfc-guide-example"
     }
   }
 }
