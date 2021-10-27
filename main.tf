5df6ca86c43197831d02b3f75147f8a6d3ce6cfb
admin1 <damir_rajabov@epam.com>
2021-10-27 15:51:16 +0500

    point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "test-endianness"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "ci-cd-github-workflow"
     }
   }
 }
