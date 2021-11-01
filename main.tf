a96e979c24fe25e985b9e5fecf5d71f3757ee775
Juraj Martinka <jumarko@gmail.com>
2021-11-01 04:32:16 +0100

    Update main.tf to use my own org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "cloud-guru-terraform-course-demo"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "cloud-guru-demo-github-actions"
     }
   }
 }
