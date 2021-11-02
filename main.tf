896a43b5ca14a4ef055e98d730835d5c0b4a32dd
gbogojevic <goranbogojevic@gmail.com>
2021-11-02 13:37:05 +0100

    change index page for EC2 instance

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello Goran" > index.html
+              echo "Hello GB" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
 }
