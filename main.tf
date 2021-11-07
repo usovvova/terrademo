394349b59b2c58a07d2032a947d1cccf903d2a5f
findfasal <findfasal@gmail.com>
2021-11-07 22:15:16 +0000

    formated the codes

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 variable "main_region" {
-   type =string 
-   default = "eu-west-2"
-  
+  type    = string
+  default = "eu-west-2"
+
 }
 provider "aws" {
-     region = var.main_region
+  region = var.main_region
 }
 module "vpc" {
-    source = "./modules/vpc"
-    region = var.main_region  
+  source = "./modules/vpc"
+  region = var.main_region
 
 }
 resource "aws_instance" "CWWebServer" {
-    ami = module.vpc.CW_ami_id
-    subnet_id = module.vpc.CW_pub_sub_id
-    instance_type ="t2.micro"   
+  ami           = module.vpc.CW_ami_id
+  subnet_id     = module.vpc.CW_pub_sub_id
+  instance_type = "t2.micro"
 }
 
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -7,10 +7,10 @@ variable "region" {
   default = "eu-west-2"
 }
 variable "vpc_cidr" {
-    type = list
-    default = ["10.0.0.0/16"]
+  type    = list(any)
+  default = ["10.0.0.0/16"]
 }
 variable "sub_cidr_public" {
-     type = list
-     default = ["10.0.1.0/24"]  
+  type    = list(any)
+  default = ["10.0.1.0/24"]
 }
