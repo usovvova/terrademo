ba33546f8bdc955b0ce7b23a09fd199ec372237b
Armin Melchior <Armin.Melchior@t-systems.com>
2021-11-18 19:21:01 +0000

    point backend to correct TFC organization and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "AMELCHIO-Terraform-Labs"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "labs-migrate-state"
     }
   }
 }
