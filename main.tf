e86d555986465e29d462f2ed3915c4705f813ecf
findfasal <findfasal@gmail.com>
2021-11-25 12:41:59 +0000

    added tag to the resource

--- main.tf
+++ main.tf
@@ -40,5 +40,8 @@ resource "aws_instance" "CWWebServer" {
   ami           = module.vpc.CW_ami_id
   subnet_id     = module.vpc.CW_pub_sub_id
   instance_type = "t2.micro"
+  tags {
+   name = "CWWebServer"
+  }
 }
 
