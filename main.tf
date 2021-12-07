66ad682d3864d7ee6034a1d4d935f231bf37e2ae
chrisgreene <chrisgreene@gmail.com>
2021-12-06 17:10:00 -0700

    kjkj

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "kHello, World" > index.html
+              echo "kkHello, World" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
 }
