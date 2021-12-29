f106c1b7163058dd7bfd5b053d90eac463a40326
Olak Banjo <kbanjo@Ola-Macs-MacBook-Pro.local>
2021-12-28 23:19:03 -0500

    new organization

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG-Terraform-demo-01"
+    organization = "AGC-Terraform-Demo"
 
     workspaces {
       name = "demo-github-actions"
