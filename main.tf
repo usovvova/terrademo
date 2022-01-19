cb8bb6876457f7f50c1680ec1fd50967c62559e4
maggit0 <maggit0@gmail.com>
2022-01-19 15:47:49 -0500

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "ACG-Terraform-Demo1966"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions:wq"
     }
   }
 }
