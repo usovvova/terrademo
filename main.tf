9f19aeb69806913c1afa83349099a32fb1b75f34
Kamil Blaz <blusik999@outlook.com>
2022-02-08 12:14:36 +0100

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-  user_data = <<-EOF
+user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
