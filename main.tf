58b83fc19f62c220c333763fb4287b2f6144b86f
Kamil Blaz <blusik999@outlook.com>
2022-02-08 12:04:28 +0100

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,17 +12,17 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "acg-terraform-blus"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-githubi-actions"
     }
   }
 }
 
 
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-central-1"
 }
 
 
