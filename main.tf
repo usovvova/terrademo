3731a3994ff0d4bb422389fdcd08c7294e237e8e
AdrienMirahi <adrien@mirahi.io>
2022-02-16 15:23:30 +0100

    test

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "qqc"
+    organization = "MirahiAdrien"
 
     workspaces {
-      name = "qqc"
+      name = "Demo_Git"
     }
   }
 }
