f808b6c8e89c966f27690a69ecf93dd8abe4e257
AdrienMirahi <98880847+AdrienMirahi@users.noreply.github.com>
2022-02-16 15:10:59 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "MirahiAdrien"
+    organization = "qqc"
 
     workspaces {
-      name = "Demo_Git"
+      name = "qqc"
     }
   }
 }
