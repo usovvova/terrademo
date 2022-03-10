2c7c1aaab68a8ee45df9eeb22f06a16fd68f5d5c
jason.northup <jason.northup@loves.com>
2022-03-10 15:27:53 -0600

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "ACG-Terraform-Labs-Ludicrous09"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "acg-demo-github-actions"
     }
   }
 }
