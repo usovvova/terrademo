55a51dbc3430d753770f4815e3e5d7ca008dfebb
harish1125 <tharish1125@gmail.com>
2022-03-23 15:59:59 +0000

    updated instance name

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ provider "aws" {
 
 resource "random_pet" "sg" {}
 
-resource "aws_instance" "web" {
+resource "aws_instance" "web1" {
   ami                    = "ami-09e67e426f25ce0d7"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
