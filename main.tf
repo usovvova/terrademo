0dbe5f20156b073a392e9b605d89258f1f85446f
harish1125 <tharish1125@gmail.com>
2022-03-23 14:13:07 +0000

    udpated the backend

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "Terraform_lab_migrate"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "Github_actions"
     }
   }
 }
