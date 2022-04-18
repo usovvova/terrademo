d01f9ad47f52567e6c627b531222d9212d6838d2
Denis Pilin <crawlerxpress@gmail.com>
2022-04-18 20:54:06 +0300

    Point remote backend to correct organisation

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "CrawlerX-Test"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "sandbox-aws"
     }
   }
 }
