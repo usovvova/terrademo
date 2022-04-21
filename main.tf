99801a7909ed6fdb268259fadb8b885f857837e2
Andrii Tymoshyk <andrii.tymoshyk@talkremit.com>
2022-04-21 15:13:50 +0300

    update org name

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "atym_test"
+    organization = "atym-test"
 
     workspaces {
       name = "demo-github-actions"
