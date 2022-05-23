e71e89ca4a32af1e7143c81135ed8c4fccf526db
José Flores <josefloressv@gmail.com>
2022-05-22 21:36:26 -0600

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-//      version = "3.26.0"
+      source = "hashicorp/aws"
+      //      version = "3.26.0"
     }
     random = {
-      source  = "hashicorp/random"
-//      version = "3.0.1"
+      source = "hashicorp/random"
+      //      version = "3.0.1"
     }
   }
   required_version = "~> 1.0"
