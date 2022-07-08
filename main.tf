d2f80da84612aa0e35e628b4b24df668039f3e1c
zenghui <huihui127127@gmail.com>
2022-07-08 15:22:43 +0100

    updated terraform version

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
+      version = "4.22.0"
     }
     random = {
       source  = "hashicorp/random"
