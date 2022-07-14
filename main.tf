68659d5c9ac6c4bc0d06d88d178b5cd995206acb
U-AzureAD\AbibOyebamiji <abiboyebamiji@gmail.com>
2022-07-14 16:23:08 -0400

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "Git_CI-CD_Demo"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }
