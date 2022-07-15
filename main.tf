821fdc8fe7e930063f2e4bc7830076cbda61d8e1
mohammad sohail <mohammadsohail009@gmail.com>
2022-07-15 06:14:07 +0000

    point backend to correct TFC and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "myterraform-cicd"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "github-actions-demo"
     }
   }
 }
