d91465c930396fea109ea2322d8bcbc2a4d1f428
Tor-Bastian Hansen <Tor-Bastian.Hansen@ufst.dk>
2022-07-27 09:16:43 +0200

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "ACG-Terraform-Demos0846"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions0846"
     }
   }
 }
