a3affd97b5bd978a6f2afd298384885c81c2ae75
Kevin Tong <kevin.tong@kevintong-ltm.internal.salesforce.com>
2022-07-30 11:55:52 -0400

    point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "kevin-terraform-demo"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "terraform-github"
     }
   }
 }
