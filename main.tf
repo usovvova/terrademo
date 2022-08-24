b7859273bff9e73413a4e62bd8e4ae61238e0cc5
Sreekanth Mukku <sreekanth_mukku@epam.com>
2022-08-24 17:06:25 +0530

    removing comment

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>3.26.0"  #commenting for initiating cicdok
+      version = "~>3.26.0"
     }
     random = {
       source  = "hashicorp/random"
