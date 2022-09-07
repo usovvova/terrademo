e081098ac6c1a97af3fded4d809a107f414315a2
Man Sinh Le <man.sinh.le@outlook.com>
2022-09-07 08:47:01 +0700

    replace new ami to dev branch

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-087c17d1fe0178315"
+  ami                    = "ami-05fa00d4c63e32376"
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["aws_security_group.web-sg.id"]
 
