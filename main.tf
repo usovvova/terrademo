7431845aabb76494cd2e2abb85c5932ff6963c8d
tfvalidator <tfvalidator@gmail.com>
2022-09-22 12:07:53 +0000

    Correct confing

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-09e67e426f25ce0d7"
+  ami                    = "ami-09e67e426f25ce0d0"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
