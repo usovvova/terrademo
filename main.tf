12821405772047b4473284b287116704a14087d5
Sanjay Jadhav <spjadhav78@gmail.com>
2022-09-23 00:33:14 +0530

    readded organization

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "spjadhav78"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }
