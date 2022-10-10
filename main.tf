dbcd2b32db20b9834081f1dafe1c0d75fe019ea6
Mauricio Benavides <mfbenavidesc@gmail.com>
2022-10-10 19:44:18 +0000

    point backend to correct TF config org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "ACG-tf-demos-mauro"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }
