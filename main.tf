464b3581780a2bc8771d608e724af71de91b90a5
Rabieh Fashwall <33328297+rfashwall@users.noreply.github.com>
2022-10-23 10:08:38 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "rf-01"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-tf-actions"
     }
   }
 }
