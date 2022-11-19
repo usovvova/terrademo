26fa1bcbe4239f09c80609a541575cbc8afd668a
EC2 Default User <ec2-user@ip-172-31-21-38.ec2.internal>
2022-11-19 17:28:04 +0000

    africa

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
   ami                    = "ami-09e67e426f25ce0d7"
-  instance_type          = "t2.medium"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
