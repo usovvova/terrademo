e59ba03eecee986cfbadc509e3b5d8ddb9acf637
ananya69821 <ananya.sakshi.srivastava@gmail.com>
2022-11-28 15:19:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,35 +4,32 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 2.26"
     }
-    random = {
+	random = {
       source  = "hashicorp/random"
       version = "3.0.1"
     }
   }
-  required_version = "~> 1.0"
 
-  backend "remote" {
-    organization = "Anu-demo"
+  required_version = ">= 0.14.9"
+}
 
-    workspaces {
-      name = "demo_github"
-    }
+backend "remote" {
+  organization = "Anu-demo"
+
+  workspaces {
+    name = "demo_github"
   }
 }
 
-
 provider "azurerm" {
   features {}
+  skip_provider_registration = true
 }
 
-
+# Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
   name                = "BatmanInc"
   address_space       = ["10.0.0.0/16"]
   location            = "Central US"
   resource_group_name = "1-132719e7-playground-sandbox"
-}
-
-output "RGname" {
-  value = "${azurerm_virtual_network.vnet.resource_group_name}"
 }
