d898d5c7f793fd9c8fe6034f2ef22ad425a21f3e
gauravgur2410 <gauravgur2410@gmail.com>
2022-12-01 11:11:03 +0530

    updated backend to correct terraform config org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "terraform-abacus"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "github-actions-abacus"
     }
   }
 }
