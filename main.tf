72c13a97b9d4517ee3042462410b9e4caec713a3
Ramón Cahenzli <ramon.cahenzli@vshn.ch>
2022-12-14 17:43:55 +0100

    Config org

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "acg-terraform-demo-psy-q"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }
