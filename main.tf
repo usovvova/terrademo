71d809b3050a3305fd43db212c756e9631a6d3e5
Joseph Awadalla <joseph.awadalla@gmail.com>
2022-12-15 21:31:45 +0000

    add org and workspace in main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,11 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "JoeA-ACG-Terraform-Demos"
+
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions-2"
     }
   }
 }
