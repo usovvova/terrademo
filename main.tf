27e9265687fd6551e9b3f9756bc9654fc697ca3b
Zekai Otles <otles2@wisc.edu>
2022-12-15 21:56:01 +0000

    Point backend correct terraform correct organization and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "ACG-Terraform-Labs-20022"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "Demo-Github-actions"
     }
   }
 }
