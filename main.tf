b4b64a80bb6cb8a727a72495b94b3f6b2d167e78
Newton Costa <newtongomez04@gmail.com>
2023-01-02 21:24:14 +0100

    Point backend to TF cloud organization and workspace

--- main.tf
+++ main.tf
@@ -11,11 +11,11 @@ terraform {
   }
   required_version = "~> 1.0"
 
-  backend "remote" {
-    organization = "REPLACE_ME"
+  cloud {
+    organization = "NC-Terraform-Labs"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }
