fd19e06d728042d4aa27088dc4e05e2b1480b34e
Home-Desktop <nhson.dt@gmail.com>
2023-01-08 13:16:34 +0700

    point backend

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "hongson89"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "hongson89-demo"
     }
   }
 }
