a745d930d7da1f33da26d2af64c102571bcfcc24
Michael Rothstein <michaelrothstein@Michaels-MacBook-Pro.local>
2023-01-10 12:50:09 -0500

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "ACG-Terraform-Demos74"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demp-github-actions"
     }
   }
 }
