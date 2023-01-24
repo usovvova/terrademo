f3c5aa7178c46ceff09a6ae3280e34bedc5d9178
Camilo Ruiz <camilorm@Camilos-Mac-mini.local>
2023-01-24 04:10:43 -0500

    Setup terraform cloud with organization and spaces

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "terraform_camiloud90"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo_workspace"
     }
   }
 }
