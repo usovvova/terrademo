fea52985161ff22d6defe69950377fd028816ced
Marcus Smith <marcsmith1280@yahoo.com>
2023-03-29 09:03:47 -0400

    point backend to correct org and workspace

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ provider "aws" {
 
 resource "random_pet" "sg" {}
 
-resource "aws_instance" "web" {
+resource "aws_instance" "web-server" {
   ami                    = "ami-00c39f71452c08778"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
