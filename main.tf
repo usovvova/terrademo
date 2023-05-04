c9eb74930b10d89b965da4c9bfbc43534b463e25
Cloud User <azureuser@acg-terraform.eahui4ocqq5ulafft20zyhybya.bx.internal.cloudapp.net>
2023-05-04 13:58:25 +0000

    Point backend to correct TFC organisation and workspace

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.31.1"
     }
     random = {
       source  = "hashicorp/random"
@@ -12,45 +12,107 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "ACG-Terraform-Demos-abdus"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }
 
 
-provider "aws" {
-  region = "us-east-1"
+provider "azurerm" {
+  features {}
+  subscription_id = "a4c81412-9cb9-4d76-aaa7-14f85696678a"
+  client_id = "15228083-4afb-4421-9826-ff06d09bfc85"
+  client_secret = "b6bf4e95-9788-43c0-a6e1-110273659091"
+  tenant_id = "ff1fc9e3-26ae-45ae-a79a-32197c30c04d"
 }
-
-
 
 resource "random_pet" "sg" {}
 
-resource "aws_instance" "web" {
-  ami                    = "ami-09e67e426f25ce0d7"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
+resource "azurerm_network_security_group" "web-sg" {
+  name                = "${random_pet.sg.id}-sg"
+  location            = "East US"
+  
+  security_rule {
+    name                       = "Allow_HTTP_Inbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "azurerm_public_ip" "web-ip" {
+  name                = "${random_pet.sg.id}-ip"
+  location            = "East US"
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "web-nic" {
+  name                = "${random_pet.sg.id}-nic"
+  location            = "East US"
+
+  ip_configuration {
+    name                          = "web-ip"
+    subnet_id                     = azurerm_subnet.web-subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.web-ip.id
+  }
+}
+
+resource "azurerm_virtual_machine" "web" {
+  name                  = "${random_pet.sg.id}-vm"
+  location              = "East US"
+  resource_group_name   = "my-resource-group"
+  network_interface_ids = [azurerm_network_interface.web-nic.id]
+  vm_size               = "Standard_B1s"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "${random_pet.sg.id}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "myvm"
+    admin_username = "adminuser"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y apache2",
+      "echo 'Hello, World' | sudo tee /var/www/html/index.html"
+    ]
+
+    connection {
+      type     = "ssh"
+      host     = azurerm_public_ip.web-ip.ip_address
+      user     = "adminuser"
+      password = "Password1234!"
+    }
   }
 }
 
 output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
+  value = "http://${azurerm_public_ip.web-ip.ip_address}:8080"
 }
