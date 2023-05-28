d9d9d9e33c53f183e4f7231bd8e5babd373a23c3
Hosung Yoon <hyoon@akamai.com>
2023-05-28 14:15:54 +0900

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "ACG-terraform-demos-7200"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-gihub-actions"
     }
   }
 }
