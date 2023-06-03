446ab29ea5e615f5cec52e654e671e4860a2f45f
markbusenhart <busenhart.clan@bluewin.ch>
2023-06-03 21:39:41 +0200

    bugfix: ACG-Terraform-...

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ASG-Terraform-Demos"
+    organization = "ACG-Terraform-Demos"
 
     workspaces {
       name = "demo-github-actions"
