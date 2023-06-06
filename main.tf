1d5f6a22c92d7b2ce71bb61b948e03f360a136f1
panche <cvetkovski_pance@yahoo.com>
2023-06-06 03:37:44 +0200

    Point backend to correct tF CONfig

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "test-demo-123"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }
