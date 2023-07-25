8c2097fe90d64c33bbbc3f0d7127c4495600852e
Ranjitha Manjunath <140109301+ranjithamanjunath21@users.noreply.github.com>
2023-07-25 09:19:35 -0400

    Revert "changed config"

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG-Terraform-Demo03"
+    organization = "REPLACE_ME"
 
     workspaces {
-      name = "Demo-GitHub-Actions"
+      name = "REPLACE_ME"
     }
   }
 }
