f7e3f9cbc7e53b73f6846b806aca019945e9c1f4
Sumit Kumar <sumit@particle41.com>
2023-08-07 23:58:31 +0530

    updated backend config

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "mimedia-terraform-poc"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "mimedia-ws-poc"
     }
   }
 }
