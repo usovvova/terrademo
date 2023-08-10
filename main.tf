246a8e210186d85c542a2981a30c88e14b5ce6ca
Phil Maitisa <73203422+boss-p@users.noreply.github.com>
2023-08-10 15:07:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG_TERRAFORM_DEMOSS"
+    organization = "jyhbkjn"
 
     workspaces {
       name = "demo-github-actions"
