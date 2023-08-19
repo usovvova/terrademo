d6619ca12fce844878ed06e508d11ec2193d6abf
Olga Kondaurov <okondaurov@bravurasolutions.com>
2023-08-19 19:02:21 +1000

    feat-xxx-111 testing

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
+      source = "hashicorp/aws"
+      version = "5.13.1"
     }
   }
-  required_version = "~> 1.0"
+}
+
+provider "aws" {
+  # Configuration options
+}
 
   backend "remote" {
     organization = "REPLACE_ME"
