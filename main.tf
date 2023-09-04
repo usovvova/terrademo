783d38170ebd016a183ca2b026b48d120f1970a8
Kookie <chiboubkaoutar@gmail.com>
2023-09-04 23:10:57 +0200

    update backend

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "Migration_Terraform"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo_github_actions"
     }
   }
 }
