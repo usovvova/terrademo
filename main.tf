1ba2d0308e5833f4c453fbd004f1cf1b14230395
swapna.pampana@gmail.com <swapna.pampana@gmail.com>
2023-09-25 19:26:55 +0000

    Point backend to correct TFC Org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG-Terraform-Demos"
+    organization = "ACG-Terraform-Demos15"
 
     workspaces {
       name = "demo-github-actions"
