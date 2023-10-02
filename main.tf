690167e5007a107402bd20979549e419b4024bf6
nvoznyuk74 <Nataliya_Voznyuk@epam.com>
2023-10-02 09:10:02 -0400

    Point backend to correct TFC org and workspace v2

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG-Terraform-Labs_nv_test"
+    organization = "ACG-Terraform-Demos-nv"
 
     workspaces {
       name = "demo-github-actions"
