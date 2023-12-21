9c81abb03039bf75350ce32fec00cd3dfbd85afc
sthird <stuart.third@live.co.uk>
2023-12-21 10:46:19 +0000

    commit

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
+      version = "5.31.0"
     }
     random = {
       source  = "hashicorp/random"
