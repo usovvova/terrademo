9de5fa23e771286b3f25a1c4abe4d122e25182ac
Pawar <mayuri.a.pawar@accenture.com>
2024-01-18 15:49:14 +0530

    new org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "ACG_git"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = demo_github_actions"
     }
   }
 }
