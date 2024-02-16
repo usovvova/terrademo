8f939603f6fdadf4b4131166a91323928a7798a4
jwaltrip4 <waltrip.alt@gmail.com>
2024-02-16 14:28:08 -0500

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,11 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "ACG-terraform-JW"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "learn-terraform-github-actions"
+
     }
   }
 }
