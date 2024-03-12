93f1c1b81cd5daaf977ff9f2094e887951bb6f4b
Carlos Tomás <fenomenostranger@gmail.com>
2024-03-12 16:57:01 +0100

    feat: changes (#9)
    Co-authored-by: Carlos Tomás <carlos.tomas@marionete.co.uk>

--- main.tf
+++ main.tf
@@ -15,21 +15,35 @@ terraform {
     organization = "hevil-terraform-labs"
 
     workspaces {
-      name = "demo-github-actions"
+      name = "terraform-web-server"
     }
   }
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region  = "eu-west-1"
 }
 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
 
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-09e67e426f25ce0d7"
-  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
