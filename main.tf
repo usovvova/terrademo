60539fd867092560935feb95ce72ce9a30c070ab
Charungooa <v-2amitc@hotmail.com>
2024-03-18 12:49:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,6 +49,12 @@ resource "aws_security_group" "web-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 output "web-address" {
