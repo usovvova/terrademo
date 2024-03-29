c4fc8b828f967e241bf002340b3959d1e405fdb9
omarkhan786 <60778560+omarkhan786@users.noreply.github.com>
2024-03-29 12:24:54 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG-Terraform-Demos"
+    organization = "OmarKhan-Terraform-Demos"
 
     workspaces {
       name = "demo-github-actions"
