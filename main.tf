24e81e04c76543320322283aa9f017ff03b037c3
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2024-04-10 12:26:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG-Terraform-Demos78"
+    organization = "REPLACE_ME"
 
     workspaces {
-      name = "demo-github-actions"
+      name = "REPLACE_ME"
     }
   }
 }
