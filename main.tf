f18abd4936f41bd474818a231962ae21c0539825
Karthik Mannepalli <mpvkarthick@gmail.com>
2024-04-20 05:22:39 +0000

    Updated org name

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG-Terraform-Demos78"
+    organization = "ACG-Terraform-Demos"
 
     workspaces {
       name = "demo-github-actions"
