719880d39410f35d01c98506fd43175bac7f3a58
jeet136@yahoo.com <jitesh.rathod@apexon.com>
2024-05-09 18:46:15 +0530

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG-Terraform-Demos78"
+    organization = "Infostretch-Corporation"
 
     workspaces {
       name = "demo-github-actions"
