1eec820513875c7a06402f7b4f411d5b9bd7a281
netshivam <shivam.agrawal@apexon.com>
2024-05-10 14:48:39 +0530

    updated

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG-Terraform-Demos78"
+    organization = "Infostretch-Corporation"
 
     workspaces {
-      name = "demo-github-actions"
+      name = "demo-github-actions-shiv"
     }
   }
 }
