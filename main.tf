6887b2c7cef576ef48e1d2b8394db177950f51f6
akshay.r <raulakys@yahoo.com>
2024-05-26 03:25:20 +0200

    update terraform main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG-Terraform-Demos"
+    organization = "Terraform-Transform-Demo"
 
     workspaces {
       name = "github-actions"
