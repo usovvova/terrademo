9530474d5f3a618a96e65d95c3d32fc9c548c5b3
Haji001 <blieye001@gmail.com>
2024-06-03 15:33:55 +0000

    point backend to org and ws

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG-Terraform-Demos78"
+    organization = "terraform-github-omoto"
 
     workspaces {
-      name = "demo-github-actions"
+      name = "github-action-terraform"
     }
   }
 }
