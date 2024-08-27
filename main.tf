cfc92adac56c000d8c6f1f714cc3d38187ca1606
dipu.mathew <cloud_user@3b343d66681c.mylabserver.com>
2024-08-27 12:10:34 +0000

    updating main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG-Terraform-Demos24"
+    organization = "ACG-Terraform-Demos25"
 
     workspaces {
       name = "demo-github-luttapi"
